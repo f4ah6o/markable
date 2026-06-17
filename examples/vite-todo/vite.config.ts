afca1ee93114c7429977b0213f9ba4c088dec5df
f12o <gh@f12o.com>
2026-06-17 10:43:03 +0900

    Vendor Vue todo example

--- examples/vite-todo/vite.config.ts
+++ examples/vite-todo/vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { markable } from "@f12o/markable/vite";
+/// <reference types="vitest" />
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import jsx from '@vitejs/plugin-vue-jsx'
+import { markable } from '@f12o/markable/vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/markable/vue-todo/" : "/",
+  base: process.env.GITHUB_ACTIONS ? '/markable/vue-todo/' : '/',
   plugins: [
     vue(),
+    jsx(),
     markable({
-      mode: "auto",
-      commentsFile: ".markable/comments.json",
-      endpoint: "/__markable/comments",
+      mode: 'auto',
+      commentsFile: '.markable/comments.json',
+      endpoint: '/__markable/comments',
     }),
   ],
-});
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+})
